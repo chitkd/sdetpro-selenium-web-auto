75ab74bcbc2a74db406ed583e6547f3a761ee0bb
KimChi2893 <dtkchi2893@gmail.com>
2023-11-11 22:22:14 +0700

    Add InterviewQuestion class of Day 12

--- src/main/java/api_learning/InterviewQuestion.java
+++ src/main/java/api_learning/InterviewQuestion.java
@@ -1,6 +1,46 @@
 package api_learning;
 
+import dev.failsafe.internal.util.Assert;
+import driver.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
 public class InterviewQuestion {
+    private static final By USERNAME_SEL = By.id("username");
+    private static final String TARGET_URL = "https://the-internet.herokuapp.com/login";
 
+    public static void main(String[] args) {
+        WebDriver driver = DriverFactory.getWebDriver();
 
+        /* Method 1:
+        Exception exception = null;
+        try {
+            // launch the page
+            driver.get(TARGET_URL);
+            WebElement usernameEle = driver.findElement(USERNAME_SEL);
+        } catch (NoSuchElementException e){
+            exception = e;
+        }
+        finally {
+            driver.quit();
+        }
+        if (exception == null){
+            System.out.println("The element ABC is on the page");
+        }
+         */
+
+        // Method 2
+        List<WebElement> usernameEles = driver.findElements(USERNAME_SEL);
+        if (!usernameEles.isEmpty()){
+            System.out.println("The element ABC is on the page");
+        }
+        else{
+            System.out.println("The element ABC is NOT on the page");
+        }
+        driver.quit();
+    }
 }
