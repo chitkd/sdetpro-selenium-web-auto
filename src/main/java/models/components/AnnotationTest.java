13b232fef15b397280b0ab927834dafefad2a9c5
KimChi2893 <dtkchi2893@gmail.com>
2023-12-07 21:34:50 +0700

    Day 15, delete AnnotationTest class & target folder

--- src/main/java/models/components/AnnotationTest.java
+++
@@ -1,7 +0,0 @@
-package models.components;
-
-public class AnnotationTest {
-    public <T> String getComponentCssSelector(Class<T> componentClass){
-        return componentClass.getAnnotation(ComponentCSSSelector.class).value();
-    }
-}
