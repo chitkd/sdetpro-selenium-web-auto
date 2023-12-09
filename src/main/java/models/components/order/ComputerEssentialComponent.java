8c881bcdc27d93b54031f694ad4b2f921b165bce
KimChi2893 <dtkchi2893@gmail.com>
2023-12-09 21:47:59 +0700

    Day 20

--- src/main/java/models/components/order/ComputerEssentialComponent.java
+++ src/main/java/models/components/order/ComputerEssentialComponent.java
@@ -1,13 +1,49 @@
 package models.components.order;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
+
 public abstract class ComputerEssentialComponent extends BaseItemComponent {
 
+    private final static By allOptionSel = By.cssSelector(".option-list input");
     public ComputerEssentialComponent(WebDriver driver, WebElement component) {
         super(driver, component);
     }
     public abstract String selectProcessorType(String type);
     public abstract String selectRAMType(String type);
+
+    public String selectHDD(String type){
+        return selectCompOption(type);
+    }
+
+    public String selectOS(String type){
+        return selectCompOption(type);
+    }
+
+    public String selectSoftware(String type){
+        return selectCompOption(type);
+    }
+
+    public void unselectDefaultOption(){
+        // lambda expression
+        component.findElements(allOptionSel).forEach(option -> {
+            if (option.getAttribute("checked") != null){
+                option.click();
+            }
+        });
+    }
+
+    protected String selectCompOption(String type){
+        String selectorStr = "//label[contains(text(),'" + type + "')]";
+        By optionSelector = By.xpath(selectorStr);
+        List<WebElement> optionEl = component.findElements(optionSelector);
+        // If we don't find that element we throw an exception to tell that the option value is not existing on the page
+        if (optionEl.isEmpty()){
+            throw new RuntimeException("[ERR] The option " + type + "is not existing to select!");
+        }
+        return optionEl.get(0).getText().trim();
+    }
 }
--- src/main/java/models/components/order/StandardComputerComponent.java
+++ src/main/java/models/components/order/StandardComputerComponent.java
@@ -1,22 +1,57 @@
 package models.components.order;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import support.ui.SelectEx;
+
+import java.util.List;
 
 public class StandardComputerComponent extends ComputerEssentialComponent{
+    private static final By productAttributeSel = By.cssSelector("select[name^='product_attribute']");
     public StandardComputerComponent(WebDriver driver, WebElement component) {
         super(driver, component);
     }
 
     @Override
     public String selectProcessorType(String type) {
-        System.out.println("selectProcessorType STANDARD");
-        return null;
+        final int PROCESS_DROPDOWN_INDEX = 0;
+        WebElement processorDropdownEle = component.findElements(productAttributeSel).get(PROCESS_DROPDOWN_INDEX);
+        return selectOption(processorDropdownEle, type);
     }
 
     @Override
     public String selectRAMType(String type) {
-        System.out.println("selectRAMType STANDARD");
-        return null;
+        final int RAM_DROPDOWN_INDEX = 1;
+        WebElement ramDropdownEle = component.findElements(productAttributeSel).get(RAM_DROPDOWN_INDEX);
+        return selectOption(ramDropdownEle, type);
+    }
+
+    private String selectOption(WebElement dropdownEle, String type){
+        SelectEx select = new SelectEx(dropdownEle);
+        List<WebElement> allOptionEles = select.getOptions();
+        String fullStrOption = null;
+
+        // Logic
+
+        // If null -> Exception
+
+        for (WebElement optionEle : allOptionEles) {
+            String currentOptionText = optionEle.getText();
+            String optionTextWithoutSpaces = currentOptionText.trim().replace(" ", "");
+            if (optionTextWithoutSpaces.startsWith(type)){
+                fullStrOption = currentOptionText;
+                break;
+            }
+        }
+
+        if (fullStrOption == null){
+            throw new RuntimeException("[ERR] The option " + type + "is not existing to select");
+        }
+
+
+        select.selectByVisibleText(fullStrOption);
+        return fullStrOption;
     }
 }
